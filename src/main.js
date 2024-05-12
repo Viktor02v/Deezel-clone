238f2bb587e5e23c7c3e1858b85190b62a3aeacd
Viktor Petruk <damnnn261@gmail.com>
2024-05-12 18:47:47 +0300

    make setups for animate.css/pinia

--- src/main.js
+++ src/main.js
@@ -1,14 +1,21 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+
+const pinia = createPinia()
+pinia.use(piniaPluginPersistedstate)
+
+import 'animate.css';
 
 import App from './App.vue'
 import router from './router'
 
+import './assets/main.css'
+
 const app = createApp(App)
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
