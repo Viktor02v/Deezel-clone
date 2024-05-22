5bc8910744c6417df45d1f0b0a308c45a42c9d94
Viktor Petruk <damnnn261@gmail.com>
2024-05-22 21:50:41 +0300

    start work on ArtistVue page layout

--- src/views/ArtistView.vue
+++ src/views/ArtistView.vue
@@ -11,11 +11,58 @@ import artist from '../artist.json'
 import { useSongStore } from '../stores/song'
 import { storeToRefs } from 'pinia'
 const useSong = useSongStore()
+
 const { isPlaying, currentTrack, currentArtist } = storeToRefs(useSong)
+
+const playFunc = () => {
+	if (currentTrack.value) {
+		useSong.playOrPauseThisSong(currentArtist.value, currentTrack.value)
+		return
+	}
+	useSong.playFromFirst()
+}
 </script>
 
 <template>
 	<div id="HeaderSection" class="max-w-[1500px] mx-auto">
+
+		<div class="flex items-center w-full relative h-full px-8 mt-6 min-w-[650px]">
+			<img width="175" class="rounded-full" :src="artist.albumCover">
+
+			<div class="ml-8">
+				<div class="text-white text-3xl w-full hover:underline cursor-pointer font-semibold">
+					{{ artist.name }}
+				</div>
+
+				<div class="text-[#bfbfbf] text-[12px] py-1.5 font-light">167,026 fans</div>
+
+				<div class="flex gap-4 items-center justify-start bottom-0 mb-1.5">
+
+					<button class="p-2.5 px-6 rounded-full bg-[#EF5465]" @click="playFunc()">
+						<div v-if="!isPlaying" class="flex items-center">
+							<Play fillColor="white" :size="20" />
+							<div class="text-white font-bold text-xs  pr-1">PLAY</div>
+						</div>
+						<div v-else class="flex items-center">
+							<Pause fillColor="white" :size="20" />
+							<div class="text-white font-bold text-xs  pr-1">Pause</div>
+						</div>
+					</button>
+
+					<button type="button" class="rounded-full p-2 border  border-[#52525D] hover:bg-[#2b2b30]">
+						<HeartOutline fillColor="#EAEAEA" :size="20" />
+					</button>
+
+					<button type="button" class="rounded-full p-2 border  border-[#52525D] hover:bg-[#2b2b30]">
+						<DotsHorizontal fillColor="#EAEAEA" :size="20" />
+					</button>
+				</div>
+			</div>
+		</div>
+
+		<div class="mt-11"></div>
+
 		
+
 	</div>
 </template>
