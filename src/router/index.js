fdac55bc431859e5d29cee1cf1e4fca2b26e5432
Viktor Petruk <damnnn261@gmail.com>
2024-05-22 21:23:41 +0300

    set up the base for the ArtistView page && set up the Pinia store and make a route to artist page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/HomeView.vue' 
+import Artist from '../views/ArtistView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,10 @@ const router = createRouter({
       name: 'home',
       component: Home
     },
+	 {
+		path: '/artist',
+      component: Artist
+	 }
   ]
 })
 
